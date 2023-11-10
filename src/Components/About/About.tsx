263ca963a198b63af27f058835441cc3b814106f
tr1pless <kadmy95@gmail.com>
2023-11-10 23:37:46 +0300

    about and textContainer updates

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,18 +1,44 @@
 import s from './about.module.css'
 import g from '../global.module.css'
 import { TextContainer } from '../TextContainer/TextContainer'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import langJson from '../../lang/lang.json'
 import { RootState } from '../store/store'
+import { nanoid } from 'nanoid'
 
 export const About = () => {
   const lang = useSelector((state: RootState) => state.counter.value)
   const data = Object.entries(langJson)
   const currentLangData: any = data[lang]
-  console.log(currentLangData[1].about.title1)
+  const title = currentLangData[1].about.title1
+  const text = currentLangData[1].about.text1
+  const text2 = currentLangData[1].about.text2
+
   return (
     <div className={`${g.background} ${s.aboutContainer}`}>
-      <TextContainer title={currentLangData[1].about.title1} />
+      <TextContainer
+        marginValue={5}
+        title={title}
+        children={<p>{text}</p>}
+        fontSize={70}
+      />
+      <TextContainer
+        fontSize={45}
+        marginValue={5}
+        title={currentLangData[1].about.title2}
+        children={
+          <div>
+            {text2.map((item: string) => {
+              return (
+                <div className={g.multiTextContainer} key={nanoid()}>
+                  <span className={g.triangleSpan}></span>
+                  <p>{item}</p>
+                </div>
+              )
+            })}
+          </div>
+        }
+      />
     </div>
   )
 }
--- src/Components/TextContainer/TextContainer.tsx
+++ src/Components/TextContainer/TextContainer.tsx
@@ -3,13 +3,27 @@ import s from './textContainer.module.css'
 
 interface Props {
   title: string
+  marginValue: number
+  children: React.ReactNode
+  fontSize: number
 }
-export const TextContainer = ({ title }: Props) => {
+export const TextContainer = ({
+  title,
+  marginValue,
+  children,
+  fontSize,
+}: Props) => {
   return (
     <>
       <div className={s.textContainer}>
         <fieldset>
-          <legend className={`${s.containerTitle} ${g.yellow}`}>{title}</legend>
+          <legend
+            style={{ marginLeft: `${marginValue}%`, fontSize: `${fontSize}px` }}
+            className={`${s.containerTitle} ${g.yellow}`}
+          >
+            {title}
+          </legend>
+          <div className={`${g.white}`}>{children}</div>
         </fieldset>
       </div>
     </>
