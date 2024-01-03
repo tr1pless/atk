3e16271db6a232fbc4ebc871fbc9f76264de809b
kadmy95@gmail.com <kadmy95@gmail.com>
2024-01-03 16:27:06 +0200

    build version prepared

--- src/Components/PriceList/PriceList.tsx
+++
@@ -1,6 +0,0 @@
-import s from './priceList.module.css'
-import g from '../global.module.css'
-
-export const PriceList = () => {
-  return <div className={`${g.background} ${s.priceListContainer}`}></div>
-}
