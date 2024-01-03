11466a812528d280bd413cacbca7dc9850bc402d
kadmy95@gmail.com <kadmy95@gmail.com>
2024-01-03 16:01:15 +0200

    smart devices adaptation done

--- src/Components/Contacts/Contacts.tsx
+++ src/Components/Contacts/Contacts.tsx
@@ -13,24 +13,32 @@ export const Contacts = () => {
   const data = Object.entries(langJson)
   const currentLangData: any = data[lang]
   const title = currentLangData[1].contacts.pageTitle
+  const mobileData = useSelector((state: RootState) => state.counter.mobileData)
 
   return (
     <div className={`${g.background} ${s.contactsContainer}`}>
       <TextContainer
         title={title}
-        fontSize={80}
+        fontSize={mobileData ? 46 : 80}
         marginValue={'0 auto 0 60px'}
         children={
           <>
             <ConctactChild />
             <div className={g.globalBtnsWrp}>
               <PageButton
-                className={g.backButton}
+                className={`${g.backButton} `}
                 rev={true}
                 to={'/prices'}
                 nav={4}
+                cClass={true}
               />
-              <PageButton className={g.nextButton} to={'/pp'} nav={7} />
+              <PageButton
+                className={`${g.nextButton} `}
+                pp={true}
+                to={'/pp'}
+                nav={7}
+                cClass={true}
+              />
             </div>
           </>
         }
